b8b540e81a203d7a50fa728ee5c922778709a76a
Mohsin Muhammad Ali <56477672+Mohsinali0786@users.noreply.github.com>
2022-11-02 13:18:53 +0500

    updated v1 02 Nov 2022

--- client/src/component/cards.js
+++ client/src/component/cards.js
@@ -10,6 +10,9 @@ import SwipeVerticalIcon from '@mui/icons-material/SwipeVertical';
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import CategoryIcon from '@mui/icons-material/Category';
 import LinearProgress from '@mui/material/LinearProgress';
+import AreaChart from '../component/charts/areaChart'
+import { fontSize, height } from '@mui/system';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 function Cards() {
 
@@ -66,80 +69,131 @@ function Cards() {
 
             </Grid>
             {/* <Box sx={{ width: '100%' }}> */}
-            <Grid container spacing={1}>
-                <Grid item xs={12} md={8} className='cards-2nd-row-cards-Grid'>
-                    <div className='cards-2nd-row-cards-parent-div'>
-                        <div>
-                            <p>Built by Developers</p>
-                            <h5>Soft UI Dashboard</h5>
-                            <p>From colors, cards, typography to complex elements, you will find the full documentation.</p>
+            <Grid container spacing={2} sx={{ marginTop: '10px' }}>
+                <Grid item xs={12} md={7} >
+                    <Grid className='cards-2nd-row-cards-Grid'>
+                        <div className='cards-2nd-row-cards-parent-div'>
+                            <div>
+                                <p>Built by Developers</p>
+                                <h5>Soft UI Dashboard</h5>
+                                <p>From colors, cards, typography to complex elements, you will find the full documentation.</p>
+                                <div className='cards-read-more-link'>
+                                    <a>Read more</a>
+                                    <ArrowRightAltIcon />
+                                </div>
+                            </div>
+                            <div className='cards-2nd-row-cards-div'>
+                                <RocketIcon className='cards-2nd-row-cards-icon' />
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} md={5}>
+                    <Grid className='cards-2nd-row-cards-Grid work-with-rocket-div'>
+                        <div className='cards-2nd-row-2nd-maindiv '>
+                            <h5>Work with the rockets</h5>
+                            <p>Wealth creation is an evolutionarily recent positive-sum game. It is all about who take the opportunity first.</p>
                             <div className='cards-read-more-link'>
                                 <a>Read more</a>
                                 <ArrowRightAltIcon />
                             </div>
                         </div>
-                        <div className='cards-2nd-row-cards-div'>
-                            <RocketIcon className='cards-2nd-row-cards-icon' />
-                        </div>
-                    </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={4} className='cards-2nd-row-cards-Grid'>
-                    <div className='cards-2nd-row-2nd-maindiv '>
-                        <h5>Work with the rockets</h5>
-                        <p>Wealth creation is an evolutionarily recent positive-sum game. It is all about who take the opportunity first.</p>
-                        <div className='cards-read-more-link'>
-                            <a>Read more</a>
-                            <ArrowRightAltIcon />
+            </Grid>
+            <Grid container spacing={2} sx={{ marginTop: '10px' }}>
+                <Grid item xs={12} md={4} >
+                    <Grid className='cards-2nd-row-cards-Grid'>
+                        <div className='cards-chart-div'>
+                            <Chart />
                         </div>
-                    </div>
-                </Grid>
-                <Grid item xs={12} md={6} className='cards-2nd-row-cards-Grid'>
-                    <div className='cards-chart-div'>
-                        <Chart />
-                    </div>
-                    <div className='cards-chartdiv-text'>
-                        <h5>Active Users</h5>
-                        <p><strong>(+23%)</strong> than last week</p>
-                    </div>
-                    <Grid container>
-
-                        <Grid item xs={12} md={6}>
-                            <div className='cards-chart--div-icons'>
-                                <div className='card-chartdiv-icons1color' >
-                                    <DescriptionIcon />
+                        <div className='cards-chartdiv-text'>
+                            <h5>Active Users</h5>
+                            <p><strong>(+23%)</strong> than last week</p>
+                        </div>
+                        <Grid container spacing={1} sx={{marginLeft:'2%',marginRight:'2%' }}>
+                            <Grid item xs={6} md={3}>
+                                <div className='cards-chart--div-icons'>
+                                    <div className='card-chartdiv-icons1color' >
+                                        <DescriptionIcon />
+                                    </div>
+                                    <p>Users</p>
                                 </div>
-                                <p>Users</p>
-                            </div>
-                            <h5>36K</h5>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <div className='cards-chart--div-icons'>
-                                <SwipeVerticalIcon />
-                                <p>Clicks</p>
-                            </div>
-                            <h5>36K</h5>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <div className='cards-chart--div-icons'>
-                                <ReceiptIcon />
-                                <p>Sales</p>
-                            </div>
-                            <h5>36K</h5>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <div className='cards-chart--div-icons'>
-                                <CategoryIcon />
-                                <p>Items</p>
-                            </div>
-                            <h5>36K</h5>
-                            <LinearProgress variant="determinate" value={50} sx={{ width: '50%' }} />
+                                <h5 className='cards-chart-numeric'>36K</h5>
+                                <LinearProgress variant="determinate" value={50} sx={{
+                                    width: '50%', backgroundColor: `silver`,
+                                    "& .MuiLinearProgress-bar": {
+                                        backgroundColor: `black`,
+                                    },
+                                }} />
 
+                            </Grid>
+                            <Grid item xs={6} md={3}>
+                                <div className='cards-chart--div-icons'>
+                                    <div className='card-chartdiv-icons2color'>
+                                        <SwipeVerticalIcon />
+                                    </div>
+                                    <p>Clicks</p>
+                                </div>
+                                <h5 className='cards-chart-numeric'>36K</h5>
+                                <LinearProgress variant="determinate" value={50} sx={{
+                                    width: '50%', backgroundColor: `silver`,
+                                    "& .MuiLinearProgress-bar": {
+                                        backgroundColor: `black`,
+                                    },
+                                }} />
+
+                            </Grid>
+                            <Grid item xs={6} md={3}>
+                                <div className='cards-chart--div-icons'>
+                                    <div className='card-chartdiv-icons3color'>
+                                        <ReceiptIcon />
+                                    </div>
+                                    <p>Sales</p>
+                                </div>
+                                <h5 className='cards-chart-numeric'>36K</h5>
+                                <LinearProgress variant="determinate" value={50} sx={{
+                                    width: '50%', backgroundColor: `silver`,
+                                    "& .MuiLinearProgress-bar": {
+                                        backgroundColor: `black`,
+                                    },
+                                }} />
+
+                            </Grid>
+                            <Grid item xs={6} md={3}>
+                                <div className='cards-chart--div-icons'>
+                                    <div className='card-chartdiv-icons4color'>
+                                        <CategoryIcon />
+                                    </div>
+                                    <p>Items</p>
+                                </div>
+                                <h5 className='cards-chart-numeric'>36K</h5>
+                                <LinearProgress variant="determinate" value={50} sx={{
+                                    width: '50%', backgroundColor: `silver`,
+                                    "& .MuiLinearProgress-bar": {
+                                        backgroundColor: `black`,
+                                    },
+                                }} />
+                            </Grid>
                         </Grid>
+
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Grid className='cards-2nd-row-cards-Grid'>
+                        <div className='cards-areachart-div'>
+                            <h4>Sales Overview</h4>
+                            <div>
+                                <ArrowUpwardIcon/>
+                                <p>4% more in 2021</p>
+                            </div>
+                            <AreaChart />
+                        </div>
                     </Grid>
                 </Grid>
             </Grid>
             {/* </Box> */}
-        </div>
+        </div >
 
 
     );
